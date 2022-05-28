3797ff9da5f5257d686b26d38ae379a955c2871b
Hardik78 <68885095+Hardik78@users.noreply.github.com>
2022-05-28 21:20:28 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Climate is:",
+    answer: "The weather pattern in a particular territory over a period of many years",
+    options: [
+      "The state of the weather in the course of a week",
+      "A feature of the weather specifying the frequency of precipitation",
+      "The weather pattern in a particular territory over a period of many years",
+      "What old people remember about weather patterns, weather lore"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Since the middle of the 19th century the air temperature on Earth",
+    answer: "Has risen",
+    options: [
+      "Has risen",
+      "Has not changed",
+      "Has fallen",
+      "Rose sharply and then fell again"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of these features is not relevant to climate?",
+    answer: "Longitude",
+    options: [
+      "The pattern of precipitation",
+      "Longitude",
+      "Dominant winds",
+      "Аir temperature"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What do we call a scientist who studies weather and climate?",
+    answer: "meteorologist",
+    options: [
+      "speleologist",
+      "geologist",
+      "meteorologist",
+      "seismologists"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following phenomena is not considered to be a consequence of climate change?",
+    answer: "Earthquakes",
+    options: [
+      "Earthquakes",
+      "Droughts",
+      "Rising ocean levels",
+      "Tornadoes"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What can you do to help fight climate change?",
+    answer: "All of the above",
+    options: [
+      "Divest from fossil fuel companies",
+      "Engage yourself in the science behind climate change",
+      "Vote for political candidates who will advocate for climate-related legislation and policy improvements",
+      "All of the above"
+    ]
+  },
+      {
+    numb: 7,
+    question: "What gases are causing current climate change?",
+    answer: "All of the above",
+    options: [
+      "Methane",
+      "Carbon Dioxide",
+      "Nitrous Oxide",
+      "All of the above"
+    ]
+  },
+      {
+    numb: 8,
+    question: "Which of the following climatic zones has wet and hot weather conditions all year round?",
+    answer: "Tropical",
+    options: [
+      "Tropical",
+      "Sub-Tropical",
+      "Equatorial",
+      "Temperate"
+    ]
+  },
+      {
+    numb: 9,
+    question: "Which climatic zone is characterized by high air temperature and much precipitation, falling evenly through the year?",
+    answer: "Equatorial",
+    options: [
+      "Tropical",
+      "Sub-Tropical",
+      "Sub-Equatorial",
+      "Equatorial"
+    ]
+  },
+      {
+    numb: 10,
+    question: "Which of the following factors has only short-term impact on climate (a few years or decade)?",
+    answer: "Volcanic eruptions",
+    options: [
+      "Change in the tilt of the Earth's axis",
+      "Continental drift",
+      "Human emissions of greenhouse gases",
+      "Volcanic eruptions"
+    ]
+  },
+      {
+    numb: 11,
+    question: "As the climate becomes warmer, the level of the world ocean:",
+    answer: "Rises",
+    options: [
+      "Remains unchanged",
+      "Rises",
+      "Falls",
+      "Cannot be predicted"
+    ]
+  },
+      {
+    numb: 12,
+    question: "Where is climate warming happening the fastest?",
+    answer: "In the Arctic",
+    options: [
+      "In Southern Europe",
+      "In tropical latitudes",
+      "In the Arctic",
+      "In the Southern Hemisphere"
+    ]
+  },
+      {
+    numb: 13,
+    question: "The most obvious sign of climate change in mountain regions is:",
+    answer: "The melting of glaciers",
+    options: [
+      "The melting of glaciers",
+      "Increase of windy days",
+      "Drying up of mountain rivers",
+      "Reduction of precipitation"
+    ]
+  },
+      {
+    numb: 14,
+    question: "In some parts of the world, rainstorms have become more intense over the past several decades. Is that a change in weather or climate?",
+    answer: "Climate",
+    options: [
+      "Weather",
+      "Climate",
+      "Both",
+      "Cannot be predicted"
+    ]
+  },
+      {
+    numb: 15,
+    question: "Which of the following control is not affecting the climate of India?",
+    answer: "Cyclones",
+    options: [
+      "Altitude",
+      "Cyclones",
+      "Latitude",
+      "Pressure & Winds"
+    ]
+  },
+      {
+    numb: 16,
+    question: "Uttar Pradesh's climate can be reffered as ______",
+    answer: "Humid Sub-Tropical Climate",
+    options: [
+      "Humid Sub-Tropical Climate",
+      "Temperate Climate",
+      "Dry Climate",
+      "Tropical Wet Climate"
+    ]
+  },
+      {
+    numb: 17,
+    question: "Steppe climate is found in which part of the Rajasthan?",
+    answer: "Between Aravalli and Western Arid Zone",
+    options: [
+      "East of Aravalli",
+      "Between Hadoti and Bhorat Plateaus",
+      "Between Aravalli and Western Arid Zone",
+      "In Southern Rajasthan"
+    ]
+  },
+      {
+    numb: 18,
+    question: "The climate of India is broadly described as _______.",
+    answer: "Monsoon type",
+    options: [
+      "Equatorial type",
+      "Polar type",
+      "Arid and semi-arid type",
+      "Monsoon type"
+    ]
+  },
+      {
+    numb: 19,
+    question: "Which of the following places experience humid climate?",
+    answer: "Kochi",
+    options: [
+      "Ahmedabad",
+      "Kochi",
+      "Ludhiana",
+      "None of the Above"
+    ]
+  },
+      {
+    numb: 20,
+    question: "Which of the following does not have an influence over the climate in India ?",
+    answer: "Ocean Currents",
+    options: [
+      "Ocean Currents",
+      "Presence of Indian Ocean",
+      "Monsoons",
+      "Nearness to equator"
+    ]
+  },
+];
